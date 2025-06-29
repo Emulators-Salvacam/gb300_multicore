35f66152778a28176ea1d73b7d72facdc9756b7d
salvacam <salvacams@gmail.com>
2025-06-29 12:57:48 +0200

    change URL toolchain

--- install-toolchain.sh
+++ install-toolchain.sh
@@ -5,5 +5,6 @@
 # Backup link for Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz:
 #      https://drive.usercontent.google.com/download?id=1MpsE__0Ra_BW3GGMfxvULeX5XeLQDSZR&authuser=0
 #
-wget https://cloud.maschath.de/s/dDrZN4DSgJ33E3w/download/Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
-tar -xzf Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
+#wget https://cloud.maschath.de/s/dDrZN4DSgJ33E3w/download/Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
+wget https://github.com/Emulators-Salvacam/gb300_multicore/releases/download/toolchain/Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.zip
+sudo unzip Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.zip -d /opt
